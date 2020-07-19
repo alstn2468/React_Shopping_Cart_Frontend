e7cabacea3a13640d04ccf786fbdf2150cf7410e
alstn2468 <alstn2468_@naver.com>
2020-07-19 17:09:35 +0900

    [#89] Add mobile warning compoennt

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import RootRouter from 'components/Router';
 import GlobalStyles from 'components/GlobalStyles';
+import Mobile from 'components/Mobile';
 
 function App(): React.ReactElement {
     return (
         <React.StrictMode>
             <GlobalStyles />
             <RootRouter />
+            <Mobile />
         </React.StrictMode>
     );
 }
