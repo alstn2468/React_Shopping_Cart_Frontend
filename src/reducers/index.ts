1cb596d5b674c3067aa7fd8a2e2bf025090f0c0f
Kimminsu <alstn2468_@naver.com>
2020-07-20 12:22:51 +0900

    [#105] Add store, combinded reducer type

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,9 +1,9 @@
-import { combineReducers } from 'redux';
+import { combineReducers, Reducer } from 'redux';
 import productListReducer from 'reducers/productListReducer';
 import cartReducer from 'reducers/cartReducer';
 import couponReducer from 'reducers/couponReducer';
 
-const rootReducer = combineReducers({
+const rootReducer: Reducer = combineReducers({
     product: productListReducer,
     cart: cartReducer,
     coupon: couponReducer,
